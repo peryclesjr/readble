06024f51aea3e59b849538f059e8f7a1dc216cd2
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-11-12 22:07:53 -0200

    ...

--- frontend/src/components/PopularPosts.js
+++ frontend/src/components/PopularPosts.js
@@ -6,7 +6,11 @@ const PopularPosts = ({ posts }) => (
       <h4>Popular Posts</h4>
     </div>
     <ul className="ul hoverable white">
-      
+      {posts.map(p => (
+        <li key={p.id} className="padding-16">
+          <span className="large">{p.title}</span>
+        </li>
+      ))}
     </ul>
   </div>
 )
