caac14d34303ef3c7dfc9ca2bd60569a939fb5ce
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-11-18 08:59:51 -0200

    Router Link ok

--- frontend/src/components/PopularPosts.js
+++ frontend/src/components/PopularPosts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import FaHeart from 'react-icons/lib/fa/heart'
 
 const PopularPosts = ({ posts }) => (
@@ -7,9 +8,12 @@ const PopularPosts = ({ posts }) => (
       <h4><FaHeart /> Popular Posts</h4>
     </div>
     <ul className="ul hoverable white">
-      {posts.map(p => (
-        <li key={p.id} className="padding-16">
-          <span className="large">{p.title}</span>
+      {posts.map(post => (
+        <li key={post.id} className="padding-16">
+          <Link
+            to={`/post/${post.id}`}>
+            <span className="large">{post.title}</span>
+          </Link>
         </li>
       ))}
     </ul>
