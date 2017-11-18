bd54b3f0666354d27a1cc811872b6a33686b7533
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-11-18 15:50:18 -0200

    add method addComment

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -3,7 +3,7 @@ import * as api from '../utils/api'
 export const ADD_COMMENT = 'ADD_COMMENT'
 export const GET_COMMENTS = 'GET_COMMENTS'
 
-export const addComment = (text, author, postId) => {
+export const addComment = (text, author = 'Anonymous', postId) => {
   return {
     type: ADD_COMMENT,
     text,
@@ -22,5 +22,12 @@ export const getComments = comments => {
 export const fetchCommentsByPost = (postId) => {
   return dispatch => {
     return api.getCommentsByPost(postId).then(comments => dispatch(getComments(comments)))
+  }
+}
+
+export const fetchAddComment = (body, author, postId) => {
+  return dispatch => {
+    cId = Math.random()
+    return api.addCommentPost(cId, body, author, postId).then(data => dispatch(addComment(body, author, postId)))
   }
 }
