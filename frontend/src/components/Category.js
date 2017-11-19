ac47e4f3fcfd50cf5f516945dea1c670e6bbd05c
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-11-19 17:30:42 -0200

    Category is now a class to connect to middleware

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,24 +1,43 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import FaTags from 'react-icons/lib/fa/tags'
 import { Link } from 'react-router-dom'
+import { fetchCategories } from '../actions/categories'
 
-const Category = ({ categories }) => (
-  <div className="card margin">
-    <div className="container padding">
-      <h4><FaTags /> Categories</h4>
-    </div>
-    <div className="container white">
-      <p>
-        {categories.map(cat => (
-          <Link key={cat.path} to={`/${cat.path}/posts`}>
-            <span className="tag light-gray small margin">
-              {cat.name}
-            </span>
-          </Link>
-        ))}
-      </p>
-    </div>
-  </div>
-)
+class Category extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchCategories())
+  }
+  render() {
+    return (
+      <div className="card margin">
+        <div className="container padding">
+          <h4><FaTags /> Categories</h4>
+        </div>
+        <div className="container white">
+          <p>
+            {this.props.categories.map(cat => (
+              <Link key={cat.path} to={`/${cat.path}/posts`}>
+                <span className="tag light-gray small margin">
+                  {cat.name}
+                </span>
+              </Link>
+            ))}
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
 
-export default Category
+Category.propTypes = {
+  categories: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  categories: state.categories.data || []
+})
+
+export default connect(mapStateToProps)(Category)
