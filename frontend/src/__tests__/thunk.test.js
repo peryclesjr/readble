6605a2160b9af2aaf0f5831c01f98f9a7cc068b8
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-11-28 15:59:00 -0200

    redux mock

---
+++ frontend/src/__tests__/thunk.test.js
@@ -0,0 +1,9 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import * as actions from '../../actions/TodoActions'
+import * as types from '../../constants/ActionTypes'
+import fetchMock from 'fetch-mock'
+import expect from 'expect' // You can use any testing library
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
