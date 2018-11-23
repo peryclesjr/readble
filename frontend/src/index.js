e3b6697b3a765b5a62bd7f075e99512d4b842516
Alberto <alberto.vieira@serpro.gov.br>
2018-11-23 15:57:06 -0200

    erro boundary

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,16 @@ import configureStore from './configureStore'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import ErrorBoundary from './utils/ErrorBoundary'
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+          <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
