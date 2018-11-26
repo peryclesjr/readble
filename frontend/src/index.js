b81f090e2c672b76bb20f3788a153c088dc24744
Alberto <alberto.vieira@serpro.gov.br>
2018-11-26 14:54:08 -0200

    service-worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,19 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import configureStore from './configureStore'
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
 import ErrorBoundary from './utils/ErrorBoundary'
+import './start-service-worker'
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL + '/'} >
       <ErrorBoundary>
           <App />
       </ErrorBoundary>
@@ -22,5 +22,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-registerServiceWorker()
